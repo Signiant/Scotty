fee4f75564c870365d0ee158eb9472a5c486d4fe
ericlaroche2 <ericlaroche2@gmail.com>
2018-04-26 15:53:46 -0400

    fixed a bug for region

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -127,19 +127,19 @@ def operations(intent_request):
             except Exception:
                 pass
 
-    if (output_session_attributes.get('is_authorized_user') is None) or ('True' not  in (output_session_attributes.get('is_authorized_user')) ) :  #intentrequest holds strings
-        return close({}, 
-                 'Fulfilled',
-                 {'contentType': 'PlainText',
-                 'content': 'You must be a valid user to use this bot. ' })
+    # if (output_session_attributes.get('is_authorized_user') is None) or ('True' not  in (output_session_attributes.get('is_authorized_user')) ) :  #intentrequest holds strings
+    #     return close({}, 
+    #              'Fulfilled',
+    #              {'contentType': 'PlainText',
+    #              'content': 'You must be a valid user to use this bot. ' })
  
     application_name = intent_request['currentIntent']['slots']['Application']
     environment_name = intent_request['currentIntent']['slots']['Environment']
     reason = intent_request['currentIntent']['slots']['Reason']
     confirm = intent_request['currentIntent']['slots']['Confirmation']
 
-    #application_name=None
-    #environment_name=None
+    #application_name=None #test: if not application_name:
+    #environment_name=None #test: if application_name and not environment_name:
     source = intent_request['invocationSource']
 
     if source == 'DialogCodeHook':
@@ -147,7 +147,7 @@ def operations(intent_request):
 
         if not application_name:
            
-            client = boto3.client('elasticbeanstalk', region_name='us-west-2') #update to handle multi env
+            client = boto3.client('elasticbeanstalk', region_name='us-east-1') #update to handle multi env
             response = client.describe_applications( )
             card=[]
             slot=[]
@@ -166,35 +166,42 @@ def operations(intent_request):
             )
             
         if application_name and not environment_name:
-            with open('config.json') as data_file:    
-                config = json.load(data_file).get('Applications')
-                
-            for region in config:
-                for app in config.get(region):
-                    if app.get('ApplicationName') == application_name:
-                        output_session_attributes['region']=region
-                        break
-            client = boto3.client('elasticbeanstalk',  region_name=output_session_attributes.get('region'))
-            response = client.describe_environments(ApplicationName=application_name)
-            card=[]
-            slot=[]
-            for env in response.get('Environments'):
-                card.append( {'text': env.get('EnvironmentName'), 'value': env.get('EnvironmentName')}     )
-                slot.append( { 'value': env.get('EnvironmentName')  } )
-            logging.critical(slot)
-
-            with open('config.json') as data_file:    
-                config = json.load(data_file).get('Applications')
-
-            running_env = get_hostedzone_balancers(config, application_name,intent_request)
-            
-            update_slot_type('ApplicationEnvironment','$LATEST', slot)  
-            
-            for pos,card_slot in enumerate(card):
-                if card_slot.get('value') == running_env:
-                    del card[pos]
-                    break
+            try:
+                with open('config.json') as data_file:    
+                    config = json.load(data_file).get('Applications')
                     
+                for region in config:
+                    for app in config.get(region):
+                        if app.get('ApplicationName') == application_name:
+                            output_session_attributes['region']=region
+                            break
+                    break
+                client = boto3.client('elasticbeanstalk',  region_name=output_session_attributes.get('region'))
+                response = client.describe_environments(ApplicationName=application_name)
+                card=[]
+                slot=[]
+                for env in response.get('Environments'):
+                    card.append( {'text': env.get('EnvironmentName'), 'value': env.get('EnvironmentName')}     )
+                    slot.append( { 'value': env.get('EnvironmentName')  } )
+                logging.critical(slot)
+    
+                with open('config.json') as data_file:    
+                    config = json.load(data_file).get('Applications')
+    
+                running_env = get_hostedzone_balancers(config, application_name,intent_request)
+                
+                update_slot_type('ApplicationEnvironment','$LATEST', slot)  
+                
+                for pos,card_slot in enumerate(card):
+                    if card_slot.get('value') == running_env:
+                        del card[pos]
+                        break
+            except Exception as e:
+                return close({}, 
+                     'Fulfilled',
+                     {'contentType': 'PlainText',
+                     'content': 'Exception '+e.format(environment_name, application_name)})
+                     
             return elicit_slot(
                 output_session_attributes,
                 intent_request['currentIntent']['name'],
@@ -289,6 +296,7 @@ def get_hostedzone_balancers(config, appname,intent_request):
                 loadbalancerdns=(response.get('ResourceRecordSets')[0].get('AliasTarget').get('DNSName'))
                 intent_request['sessionAttributes']['recordHostedZone']= (response.get('ResourceRecordSets')[0].get('AliasTarget').get('HostedZoneId'))
                 break
+        break
     intent_request['sessionAttributes']['region']=region  
     
     return get_running_env(region, loadbalancerdns)  
@@ -296,15 +304,18 @@ def get_hostedzone_balancers(config, appname,intent_request):
 
 def get_running_env(region, loadbalancerdns) : 
     client = boto3.client('elb', region_name=region) 
+    logger.critical(region)
     paginator = client.get_paginator('describe_load_balancers')
     page_iterator = paginator.paginate()
     for page in page_iterator:
         for loadbalancer in page.get('LoadBalancerDescriptions'):
+            #logger.critical(loadbalancer.get('DNSName').lower())
             if loadbalancer.get('DNSName').lower() in loadbalancerdns:
                 tagresponse = client.describe_tags(LoadBalancerNames=[loadbalancer.get('LoadBalancerName')])
                 tags = tagresponse.get('TagDescriptions')[0].get('Tags')
                 for tag in tags:
                     if tag.get('Key') == 'elasticbeanstalk:environment-name':
+                        logger.critical(tag.get('Value'))
                         return tag.get('Value') #environment-name
                     
 
